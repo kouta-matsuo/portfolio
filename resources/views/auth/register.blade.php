138da1d78b4e6cd3620824a08add573e69e91fcd
Ubuntu <ubuntu@ip-172-31-37-124.ap-northeast-1.compute.internal>
2021-09-20 00:14:58 +0000

    register.blade.php-bem-completed

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,34 +2,34 @@
 
 @section('content')
 
-<div class="text-center">
-    <h1>新規登録</h1>
+<div class="register-top">
+    <h1 class="register-top__heading">新規登録</h1>
 </div>
 
 <div class="register-form">
     
     {!! Form::open(['route' => 'signup.post']) !!}
-                <div class="form-group">
-                    {!! Form::label('name', 'お名前') !!}
-                    {!! Form::text('name', null, ['class' => 'form-control']) !!}
+                <div class="register-form__form-group">
+                    {!! Form::label('name', 'お名前', ['class' => "register-form__form-group__label"]) !!}
+                    {!! Form::text('name', null, ['class' => 'register-form__form-group__form-area form-control']) !!}
                 </div>
 
-                <div class="form-group">
-                    {!! Form::label('email', 'メールアドレス') !!}
-                    {!! Form::email('email', null, ['class' => 'form-control']) !!}
+                <div class="register-form__form-group">
+                    {!! Form::label('email', 'メールアドレス',['class' => "register-form__form-group__label"]) !!}
+                    {!! Form::email('email', null, ['class' => 'register-form__form-group__form-area form-control']) !!}
                 </div>
 
-                <div class="form-group">
-                    {!! Form::label('password', 'パスワード') !!}
-                    {!! Form::password('password', ['class' => 'form-control']) !!}
+                <div class="register-form__form-group">
+                    {!! Form::label('password', 'パスワード', ['class' => "register-form__form-group__label"]) !!}
+                    {!! Form::password('password', ['class' => 'register-form__form-group__form-area form-control']) !!}
                 </div>
 
-                <div class="form-group last">
-                    {!! Form::label('password_confirmation', '確認用パスワード') !!}
-                    {!! Form::password('password_confirmation', ['class' => 'form-control']) !!}
+                <div class="register-form__form-group register-form__form-group--padding">
+                    {!! Form::label('password_confirmation', '確認用パスワード', ['class' => "register-form__form-group__label"]) !!}
+                    {!! Form::password('password_confirmation', ['class' => 'register-form__form-group__form-area form-control']) !!}
                 </div>
 
-                {!! Form::submit('新規登録', ['class' => 'btn btn-primary btn-register']) !!}
+                {!! Form::submit('新規登録', ['class' => 'btn btn-primary register-form__btn']) !!}
             {!! Form::close() !!}
         </div>
 </div>
