d863f40a8f2190dc35f1bdd3b4a746995450920d
Ubuntu <ubuntu@ip-172-31-37-124.ap-northeast-1.compute.internal>
2021-09-20 03:08:09 +0000

    create.blade.php-bem-completed

--- resources/views/micropost/create.blade.php
+++ resources/views/micropost/create.blade.php
@@ -3,26 +3,26 @@
 @section('content')
 
 <div class="create-form">
-    <h2>新規投稿</h2>
+    <h2 class="create-form__heading">新規投稿</h2>
     
     {!! Form::model($micropost, ['route' => 'microposts.store']) !!}
 
-                <div class="form-group">
-                    {!! Form::label('from', '都道府県:') !!}
-                    {!! Form::text('from', null, ['class' => 'form-control']) !!}
+                <div class="create-form__form-group">
+                    {!! Form::label('from', '都道府県:',['class' => "create-form__form-group__label"]) !!}
+                    {!! Form::text('from', null, ['class' => 'create-form__form-group__form-area form-control']) !!}
                 </div>
                 
-                <div class="form-group">
-                    {!! Form::label('facility', '施設名:') !!}
-                    {!! Form::text('facility', null, ['class' => 'form-control']) !!}
+                <div class="create-form__form-group">
+                    {!! Form::label('facility', '施設名:',['class' => "create-form__form-group__label"]) !!}
+                    {!! Form::text('facility', null, ['class' => 'create-form__form-group__form-area form-control']) !!}
                 </div>
                     
-                <div class="form-group">    
-                    {!! Form::label('content', '説明:') !!}
-                    {!! Form::textarea('content', null, ['class' => 'form-control']) !!}
+                <div class="create-form__form-group">    
+                    {!! Form::label('content', '説明:',['class' => "create-form__form-group__label"]) !!}
+                    {!! Form::textarea('content', null, ['class' => 'create-form__form-group__form-area form-control']) !!}
                 </div>
 
-                {!! Form::submit('投稿', ['class' => 'btn btn-primary btn-create']) !!}
+                {!! Form::submit('投稿', ['class' => 'btn btn-primary create-form__create-btn']) !!}
 
     {!! Form::close() !!}
 </div>
