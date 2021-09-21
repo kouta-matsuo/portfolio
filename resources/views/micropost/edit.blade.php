d53f46dae7f8ce4ffe823aea524db7fc9e90963a
Ubuntu <ubuntu@ip-172-31-37-124.ap-northeast-1.compute.internal>
2021-09-21 03:43:26 +0000

    edit-bem-structure-change

--- resources/views/micropost/edit.blade.php
+++ resources/views/micropost/edit.blade.php
@@ -7,18 +7,18 @@
     {!! Form::model($micropost, ['route' => ['microposts.update', $micropost->id], 'method' => 'put']) !!}
 
                 <div class="edit-form__form-group">
-                    {!! Form::label('from', '都道府県:',['class' => "edit-form__form-group__label"]) !!}
-                    {!! Form::text('from', null, ['class' => 'edit-form__form-group__form-area form-control']) !!}
+                    {!! Form::label('from', '都道府県:',['class' => "edit-form__form-group-label"]) !!}
+                    {!! Form::text('from', null, ['class' => 'edit-form__form-group-form-area form-control']) !!}
                 </div>
                 
                  <div class="edit-form__form-group">
-                    {!! Form::label('facility', '施設名:',['class' => "edit-form__form-group__label"]) !!}
-                    {!! Form::text('facility', null, ['class' => 'edit-form__form-group__form-area form-control']) !!}
+                    {!! Form::label('facility', '施設名:',['class' => "edit-form__form-group-label"]) !!}
+                    {!! Form::text('facility', null, ['class' => 'edit-form__form-group-form-area form-control']) !!}
                 </div>
 
                 <div class="edit-form__form-group">
-                    {!! Form::label('content', '説明:',['class' => "edit-form__form-group__label"]) !!}
-                    {!! Form::textarea('content', null, ['class' => 'edit-form__form-group__form-area form-control']) !!}
+                    {!! Form::label('content', '説明:',['class' => "edit-form__form-group-label"]) !!}
+                    {!! Form::textarea('content', null, ['class' => 'edit-form__form-group-form-area form-control']) !!}
                 </div>
 
                 {!! Form::submit('更新', ['class' => 'btn btn-primary edit-form__edit-btn']) !!}
